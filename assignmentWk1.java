f2530b3712b112a493875bd593d09d91afa4d5da
eduper1 <eduper1.havard@gmail.com>
2024-04-18 21:41:14 +0300

    question string

---
+++ assignmentWk1.java
@@ -0,0 +1,17 @@
+/**
+ * assignmentWk1
+ */
+public class assignmentWk1 {
+
+    public static void main(String[] args) {
+        int score;
+        int numberOfQuestions;
+
+        String[] questions = {
+            "What is the largest desert in Africa?",
+            "Which river is the longest in Africa?",
+            "Which African country is known as the 'Rainbow Nation'?",
+            "What is the tallest mountain in Africa?"
+        };
+    }
+}
